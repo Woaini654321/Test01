eb689d2e31e48b12654f5f731f245a71f1def2e2
Yan <Yan@YandeMacBook-Pro.local>
2018-07-03 19:51:37 +0800

    xx

---
+++ Test01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test01
+//
+//  Created by Yan on 2018/7/3.
+//  Copyright © 2018年 Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test01
+//
+//  Created by Yan on 2018/7/3.
+//  Copyright © 2018年 Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
